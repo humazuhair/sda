9b64515d0e6e989a9a3d1fef31c7c521b09e5514
david <david@lipn.fr>
2019-09-10 09:33:55 +0200

    Commentaires python

--- TP1/Python/main.py
+++ TP1/Python/main.py
@@ -6,21 +6,21 @@ from analyzer import Analyzer
 
 # Tableau dynamique.
 a = ArrayListProxy()
-# Analyse du temps pris par les opérations.
+# Analyse du temps pris par les operations.
 time_analysis = Analyzer();
 
 for i in range(1000000):
     before = time.time()
     a.append(i)
     after = time.time()
-    # Enregistrement du temps pris par l'opération
+    # Enregistrement du temps pris par l'operation
     time_analysis.append((after - before)*10**9)
 
-# Affichage de quelques statistiques sur l'expérience.
+# Affichage de quelques statistiques sur l'experience.
 sys.stderr.write("Total cost : " + str(time_analysis.get_total_cost())+"\n")
 sys.stderr.write("Average cost : " + str(time_analysis.get_average_cost())+"\n")
 sys.stderr.write("Variance :" + str(time_analysis.get_variance())+"\n")
 sys.stderr.write("Standard deviation :" + str(time_analysis.get_standard_deviation())+"\n")
 
-// Sauvegarde les données de l'expérience: temps et temps amorti par opération.
+# Sauvegarde les donnees de l'experience.
 time_analysis.save_values("../dynamic_array_time_python.plot")
