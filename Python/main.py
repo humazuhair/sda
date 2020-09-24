0e684e2118a08873fbb65c2f7d0153e180487e58
ortogono <nadi.tomeh@gmail.com>
2020-09-24 14:28:34 +0200

    Problème allocation de mémoire dans le main du Python

--- Python/main.py
+++ Python/main.py
@@ -10,34 +10,37 @@ a = []
 time_analysis = Analyzer();
 # Analyse de la mémoire gaspillée à un instant t.
 memory_analysis = Analyzer();
-# Analyse du nombre de copies effectuées à un instant t.
+# Analyse du nombre de copies effectuées.
 copy_analysis = Analyzer();
 
 
 # Taille supposée de l'en-tête d'une list en Python
-__list_header_size__ = 56
+__list_header_size__ = 64
 # Taille supposée d'une entrée dans une list en Python
 __list_entry_size__ = 8
 
 wasted_memory = sys.getsizeof(a)-__list_header_size__
+prev_sizeof_a = sys.getsizeof(a)
 
-for i in range(100):
-    previous_size = sys.getsizeof(a)
+for i in range(1000000):
     before = time.time()
     a.append(i)
     after = time.time()
 
     # Enregistrement du temps pris par l'operation
     time_analysis.append((after - before)*10**9)
+   
+    # Enregistrement de l'espace mémoire gaspillé à un instant donné
+    wasted_memory = sys.getsizeof(a)-__list_header_size__ - __list_entry_size__*(i+1)
+
     # Enregistrement du nombre de copie effectuées par l'opération
     # S'il n'y avait pas d'espace gaspillé à l'étape précédente, 
     # alors une réallocation a eu lieu à cette étape.
-    if sys.getsizeof(a) > previous_size: 
+    if sys.getsizeof(a) > prev_sizeof_a: 
         copy_analysis.append(i+1)
     else:
         copy_analysis.append(1)
-    # Enregistrement de l'espace mémoire gaspillé à un instant donné
-    wasted_memory = sys.getsizeof(a)-__list_header_size__ - __list_entry_size__*(i+1)
+    prev_sizeof_a = sys.getsizeof(a)
     memory_analysis.append(wasted_memory)
 
 # Affichage de quelques statistiques sur l'experience.
@@ -47,6 +50,7 @@ sys.stderr.write("Variance :" + str(time_analysis.get_variance())+"\n")
 sys.stderr.write("Standard deviation :" + str(time_analysis.get_standard_deviation())+"\n")
 
 # Sauvegarde les donnees de l'experience.
-time_analysis.save_values("../plots/dynamic_array_time_python_alpha_2.plot")
-memory_analysis.save_values("../plots/dynamic_array_memory_python_alpha_2.plot")
-copy_analysis.save_values("../plots/dynamic_array_copy_python_alpha_2.plot")
+time_analysis.save_values("../plots/dynamic_array_time_python.plot")
+memory_analysis.save_values("../plots/dynamic_array_memory_python.plot")
+copy_analysis.save_values("../plots/dynamic_array_copy_python.plot")
+
