cfd8347d200942cf64b9c4e7b35a249e6c73e00b
Nassima KADOUN <11707834@f203-16.ig-edu.univ-paris13.fr>
2021-10-01 12:52:00 +0200

    Deuxième expérience

--- AUTHORS.md
+++ AUTHORS.md
@@ -1,2 +1,3 @@
+alpha=3.5
 Diallo Mohamed Bobo 11710212
 Kadoun Nassima 11707834
--- launch_analysis.sh
+++ launch_analysis.sh
@@ -16,44 +16,7 @@ make clean
 
 cd ..
 
-# Compilation et exécution en C++:
-echo "${RED} Compilation du code source en C++ ${NC}"
-cd CPP/
 
-make
-
-echo "${RED} Exécution des benchmarks en C++ ${NC}"
-./arraylist_analysis
-
-echo "${RED} Nettoyage ${NC}"
-make clean
-
-cd ..
-
-# Compilation et exécution en Java:
-
-echo "${RED} Compilation du code source en Java ${NC}"
-cd Java
-
-javac *
-
-echo "${RED} Exécution des benchmarks en Java ${NC}"
-java Main
-
-echo "${RED} Nettoyage ${NC}"
-rm *.class
-
-cd ..
-
-# Exécution en Python
-
-echo "${RED} Compilation du code source en Python ${NC}"
-cd Python
-
-echo "${RED} Exécution des benchmarks en Python ${NC}"
-python main.py
-
-cd ..
 
 # Observation des résultats
 ### Dans le répertoire plots, lisez le contenu du fichier plot_result
@@ -63,6 +26,6 @@ more plot_result
 
 ### puis lancez ce script avec gnuplot
 
-gnuplot plot_result
+gnuplot my_plot_result
 
 
